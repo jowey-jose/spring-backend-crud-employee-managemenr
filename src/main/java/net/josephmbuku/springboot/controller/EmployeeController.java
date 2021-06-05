9e5bb5db72cedae17f5461f4000a206ec5100e4b
jowey-jose <josephmbuku17@gmail.com>
2021-06-05 16:22:15 +0300

    feat(app): Creating Find Employee By Id Rest API

--- src/main/java/net/josephmbuku/springboot/controller/EmployeeController.java
+++ src/main/java/net/josephmbuku/springboot/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package net.josephmbuku.springboot.controller;
 
+import net.josephmbuku.springboot.exception.ResourceNotFoundException;
 import net.josephmbuku.springboot.model.Employee;
 import net.josephmbuku.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,4 +29,18 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+    // Get Employee by Id Rest Api
+    @GetMapping("/employees/{id}")
+    // Path Variable maps the id to the path.
+    public ResponseEntity <Employee> getEmployeeById(@PathVariable Long id){
+        // Create an employee object.
+        // Find By Id returns an optional. An optional has throw exception option.
+        // Then throw an exception if record not found.
+        // Use a lambda expression with the orElseThrow Exception.
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id:" + id));
+
+        // Return Http Status.
+        return ResponseEntity.ok(employee);
+    }
 }
